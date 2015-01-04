5d6eda4b3e47fa9633b3a2ad9c5cf11268d0362a
Magnus Morton <magnusmorton@gmail.com>
2015-01-04 21:47:21 +0000

    removed init from simple

--- pycket/analysis/simple.py
+++ pycket/analysis/simple.py
@@ -2,10 +2,6 @@ from pycket.analysis.base import Analysis
 
 class Simple(Analysis):
     
-    def __init__(self):
-        super(Analysis, self).__init__()
-        self.trace = None
-
     def cost(self):
         """ computes the cost of the analysis"""
         return len(self.trace)
