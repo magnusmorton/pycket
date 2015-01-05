51121503001489b1aafc63e806357066e5559177
Magnus Morton <magnusmorton@gmail.com>
2015-01-05 02:12:27 +0000

    namespace issues

--- pycket/prims/input_output.py
+++ pycket/prims/input_output.py
@@ -764,10 +764,10 @@ def jit_stats(args):
     print "TIMES: "
     print jit_hooks.stats_get_loop_run_times(None)
     print "COUNTERS: "
-    for i, counter_name in enumerate(Counters.counter_names):
+    for i, counter_name in enumerate(jit.Counters.counter_names):
         v = jit_hooks.stats_get_counter_value(None, i)
         print v
-        tr_time = jit_hooks.stats_get_times_value(None, Counters.TRACING)
+        tr_time = jit_hooks.stats_get_times_value(None, jit.Counters.TRACING)
         print "TRACING: "
         print tr_time
 
