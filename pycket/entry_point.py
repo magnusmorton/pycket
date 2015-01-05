ecf844b88b451a03dde206572a3aaeabf2437a47
Magnus Morton <magnusmorton@gmail.com>
2015-01-05 04:02:52 +0000

    ...

--- pycket/entry_point.py
+++ pycket/entry_point.py
@@ -45,7 +45,7 @@ def make_entry_point(pycketconfig=None):
             print "TIMES: "
             ll_times =  jit_hooks.stats_get_loop_run_times(None)
             for i in range(len(ll_times)):
-                print ll_times[i]
+                print str(ll_times[i])
             print "COUNTERS: "
             for i, counter_name in enumerate(Counters.counter_names):
                 v = jit_hooks.stats_get_counter_value(None, i)
