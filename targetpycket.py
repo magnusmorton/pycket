cc8db54e655e3d1907750b494ca657607fa67658
Magnus Morton <magnusmorton@gmail.com>
2015-01-05 18:05:10 +0000

    Revert "added JitPolicy to top level"
    This reverts commit 9e07b9b9bae3227c683beb4b044448cb68c3d469.

--- targetpycket.py
+++ targetpycket.py
@@ -3,13 +3,9 @@
 #
 
 from pycket.entry_point import target, get_additional_config_options, take_options
-from rpython.jit.codewriter.policy import JitPolicy
-
 
 if __name__ == '__main__':
     from pycket.__main__ import main
     main()
 
 #
-def jitpolicy(driver):
-    return JitPolicy()
