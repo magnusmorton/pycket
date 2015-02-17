c0a8a58593e57429c96f2c71db659e3b925d55f7
Magnus Morton <magnusmorton@gmail.com>
2015-02-17 13:47:12 +0000

    added trace hashing

--- pycket/hooks.py
+++ pycket/hooks.py
@@ -1,6 +1,6 @@
 from rpython.rlib import jit_hooks
 from rpython.rlib.jit import JitHookInterface, Counters
-from rpython.rlib.objectmodel import compute_unique_id, current_object_addr_as_int
+from rpython.rlib.objectmodel import compute_unique_id, current_object_addr_as_int, compute_hash
 
 
 from pycket.error import SchemeException
@@ -15,19 +15,21 @@ class PycketJitInterface(JitHookInterface):
         
 
     def after_compile(self, debug_info):
-        print "AFTER COMPILE"
+        print "LOOP"
         # assume first instruction is label
         current_label = None
         start_pos = 0
         for i, op in enumerate(debug_info.operations):
             if op.getopname() == "label":
                 if current_label is not None:
+                    print "HASH: ", loop_hash(debug_info.operations[start_pos:i])
                     print "TT: ", current_label.getdescr().repr_of_descr()
                     print "COST: ", str(self.analysis.cost(debug_info.operations[start_pos:i]))
                 current_label = op
                 start_pos = i
             if op.getopname() == "jump":
                 if current_label is not None:
+                    print "HASH: ", loop_hash(debug_info.operations[start_pos:i]) 
                     print "TT: ", current_label.getdescr().repr_of_descr()
                     print "COST: ", str(self.analysis.cost(debug_info.operations[start_pos:i]))
 
@@ -36,9 +38,17 @@ class PycketJitInterface(JitHookInterface):
 
     def after_compile_bridge(self, debug_info):
         # in the benchmarks I use, hopefully I won't see this
-        print "BRIDGE!! "
+        print "BRIDGE "
+        print "HASH: ", loop_hash(debug_info.operations)
         print "GUARD: ", compute_unique_id(debug_info.fail_descr)
         print "COST: ", str(self.analysis.cost(debug_info.operations))
 
 
-        
+
+
+def loop_hash(operations):
+    hash_num = 0
+    for op in operations:
+        hash_num *= 251
+        hash_num += compute_hash(op.__class__.__name__)
+    return hash_num
