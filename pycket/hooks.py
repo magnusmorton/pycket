ebff981740aae2e8a956fb69a96c7237a9596c0d
Magnus Morton <j.morton.2@research.gla.ac.uk>
2015-02-19 17:29:07 +0000

    created types for trace and bridge; moved analysis out

--- pycket/hooks.py
+++ pycket/hooks.py
@@ -5,6 +5,25 @@ from rpython.rlib.objectmodel import compute_unique_id, current_object_addr_as_i
 
 from pycket.error import SchemeException
 
+class Trace(object):
+    def __init__(self, ops):
+        self.operations = ops
+        
+    def __hash__(self):
+        hash_num = 0
+        for op in self.operations:
+            hash_num *= 251
+            hash_num += compute_hash(op.__class__.__name__)
+        return hash_num
+
+class Bridge(Trace):
+    def __init__(self, ops, guard):
+        # for some reason this doesn't work
+        #super(Bridge, self).__init__()
+        self.ops = ops
+        self.guard = guard
+
+trace_list = []
 
 class PycketJitInterface(JitHookInterface):
 
@@ -15,21 +34,12 @@ class PycketJitInterface(JitHookInterface):
         
 
     def after_compile(self, debug_info):
-        current_label = None
-        start_pos = 0
-        for i, op in enumerate(debug_info.operations):
-            if op.getopname() == "label":
-                if current_label is not None:
-                    print "LOOP - HASH: ", loop_hash(debug_info.operations[start_pos:i]), " TT: ", current_label.getdescr().repr_of_descr(), " COST: ", str(self.analysis.cost(debug_info.operations[start_pos:i]))
-                current_label = op
-                start_pos = i
-            if op.getopname() == "jump":
-                if current_label is not None:
-                    print "LOOP - HASH: ", loop_hash(debug_info.operations[start_pos:i]), " TT: ", current_label.getdescr().repr_of_descr(), " COST: ", str(self.analysis.cost(debug_info.operations[start_pos:i]))
-
+       trace_list.append(Trace(debug_info.operations))
 
     def after_compile_bridge(self, debug_info):
-        print "BRIDGE -  HASH: ", loop_hash(debug_info.operations), " GUARD: ", compute_unique_id(debug_info.fail_descr), " COST: ", str(self.analysis.cost(debug_info.operations))
+        trace_list.append(Bridge(debug_info.operations, compute_unique_id(debug_info.fail_descr)))
+
+#        print "BRIDGE -  HASH: ", loop_hash(debug_info.operations), " GUARD: ", compute_unique_id(debug_info.fail_descr), " COST: ", str(self.analysis.cost(debug_info.operations))
 
 
 
@@ -40,3 +50,17 @@ def loop_hash(operations):
         hash_num *= 251
         hash_num += compute_hash(op.__class__.__name__)
     return hash_num
+
+
+def analyse():
+    current_label = None
+    start_pos = 0
+    for i, op in enumerate(debug_info.operations):
+        if op.getopname() == "label":
+            if current_label is not None:
+                print "LOOP - HASH: ", loop_hash(debug_info.operations[start_pos:i]), " TT: ", current_label.getdescr().repr_of_descr(), " COST: ", str(self.analysis.cost(debug_info.operations[start_pos:i]))
+            current_label = op
+            start_pos = i
+        if op.getopname() == "jump":
+            if current_label is not None:
+                print "LOOP - HASH: ", loop_hash(debug_info.operations[start_pos:i]), " TT: ", current_label.getdescr().repr_of_descr(), " COST: ", str(self.analysis.cost(debug_info.operations[start_pos:i]))
