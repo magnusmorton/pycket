f5ed76fa7aa4c5b7a397fa9932298569213100c1
Magnus Morton <j.morton.2@research.gla.ac.uk>
2015-02-27 11:09:07 +0000

    plotting

--- plot.pl
+++ plot.pl
@@ -1,5 +1,5 @@
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5 
-plot 'results.plt' with linespoints ls 1
+plot 'whole_program.plt' with linespoints ls 1
 set term png
 set output "simple.png"
 replot
