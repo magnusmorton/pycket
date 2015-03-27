fcbdd191acbc05f22db6882301f6f01e9e8acc9e
Magnus Morton <magnusmorton@gmail.com>
2015-03-27 11:55:53 +0000

    prelim generated stuff

---
+++ runner.fish
@@ -0,0 +1,10 @@
+#!/usr/local/bin/fish
+
+rm output/*
+for x in (seq 1 10  1500)
+	sed s/Z/$x/ < trace-benches/generator.rkt.t > generated.rkt
+	for i in (seq 10)
+		./pycket-c generated.rkt 10000 10000 >> output/generated$x
+	end
+
+end
