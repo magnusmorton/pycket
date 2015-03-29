2e8f32da79cf03a722edc6d95596dad015725b1e
Magnus Morton <magnusmorton@gmail.com>
2015-03-30 00:47:44 +0100

    testing updates

--- runner.fish
+++ runner.fish
@@ -4,7 +4,7 @@ rm output/*
 for x in (seq 1 10  1500)
 	sed s/Z/$x/ < trace-benches/generator.rkt.t > generated.rkt
 	for i in (seq 10)
-		./pycket-c generated.rkt 10000 10000 >> output/generated$x
+		./pycket-c generated.rkt 10000 10000 >> output/generated_all$x
 	end
 
 end
