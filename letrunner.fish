2c27de972f585876af6f2a0fb10c11860553254e
Magnus Morton <magnusmorton@gmail.com>
2015-04-23 23:54:16 +0100

    fixed sed issues

--- letrunner.fish
+++ letrunner.fish
@@ -3,8 +3,8 @@
 rm output/*
 for x in (seq 1 10  1500)
         echo $x
-	set y math '$x - 1'
-	sed -e 's/X/$x/' -e 's/Y/$y' < trace-benches/arith.rkt.t > generated_let.rkt
+	set y (math "$x-1")
+	sed -e s/X/$x/ -e s/Y/$y/ < trace-benches/arith.rkt.t > generated_let.rkt
 	for i in (seq 10)
                 echo $i
 		./pycket-c generated_let.rkt 10000 10000 >> output/generated_all$x
