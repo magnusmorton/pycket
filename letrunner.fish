12c6060fd894979c75439ac635d7a556d0ab11f1
Magnus Morton <m@magnusmorton.com>
2015-05-12 15:10:31 +0100

    changed output file name

--- letrunner.fish
+++ letrunner.fish
@@ -7,7 +7,7 @@ for x in (seq 1 10  1500)
 	sed -e s/X/$x/ -e s/Y/$y/ < trace-benches/arith.rkt.t > generated_let.rkt
 	for i in (seq 10)
                 echo $i
-		./pycket-c generated_let.rkt 10000 10000 >> output/generated_all$x
+		./pycket-c generated_let.rkt 10000 10000 >> output/generated_let$x
 	end
 
 end
